6acd21b4cc21a5852127361252bbb11f4856ba2c
Manuel Bogino <manuelbogino@gmail.com>
2023-06-15 15:58:17 -0300

    Add search input

--- components/features/NftList/NftList.tsx
+++ components/features/NftList/NftList.tsx
@@ -1,12 +1,17 @@
 import { fetchNfts } from "@/lib/fetchNfts";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { Fragment, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { ItemBox } from "./components";
 import { Spinner } from "@/components/ui/Spinner";
+import { Input } from "@/components/ui/Input";
+import _debounce from "lodash.debounce";
 
 export function NftList() {
   const { ref, inView } = useInView();
+  const [filter, setFilter] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [filteredData, setFilteredData] = useState<NFT[]>([]);
 
   const { status, data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
     ["nfts"],
@@ -21,24 +26,48 @@ export function NftList() {
   );
 
   useEffect(() => {
-    if (inView) {
+    if (inView && !filter) {
       fetchNextPage();
     }
-  }, [inView]);
+  }, [inView, filter]);
+
+  const debounceFilter = useCallback(_debounce(setFilter, 1000), []);
+
+  function handleChange({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(value);
+    debounceFilter(value);
+  }
+
+  useEffect(() => {
+    if (data) {
+      const flattenedData = data.pages.flatMap((page) => page.results);
+      const filteredItems = flattenedData.filter((item) =>
+        item.title.toLowerCase().includes(filter.toLowerCase())
+      );
+      setFilteredData(filteredItems);
+    }
+  }, [data, filter]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="p-8 flex min-h-screen flex-col items-center justify-between md:p-24">
       {status === "loading" ? (
         <Spinner />
       ) : (
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-6 lg:grid-cols-4 lg:gap-10">
-          {data?.pages.map((page, index) => (
-            <Fragment key={`page-${index}`}>
-              {page.results.map((item) => (
-                <ItemBox key={item.id} item={item} />
-              ))}
-            </Fragment>
-          ))}
+        <div>
+          <div className="w-full md:w-96">
+            <Input
+              value={inputValue}
+              placeholder="Search nft name"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="grid grid-cols-1 gap-5 mt-8 md:grid-cols-2 md:gap-6 lg:grid-cols-4 lg:gap-10">
+            {filteredData.map((item) => (
+              <ItemBox key={item.id} item={item} />
+            ))}
+          </div>
         </div>
       )}
       <div ref={ref} />
---
+++ components/ui/Input/Input.tsx
@@ -0,0 +1,14 @@
+import { InputHTMLAttributes } from "react";
+
+export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <div>
+      <div className="mt-2">
+        <input
+          className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          {...props}
+        />
+      </div>
+    </div>
+  );
+}
